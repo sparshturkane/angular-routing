f77ed9ff1b7a2de8f978ce19b372f312d79c7ea2
sparshturkane <rajaturkane99@gmail.com>
2018-08-16 19:21:29 +0530

    added about and fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 const routes: Routes = [
-  {path: '', component: AppComponent},
-  {path: 'footer', component: FooterComponent}
+  {path: 'about', component: AboutComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AppRoutingModule } from './app-routing.module';
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    BannerComponent
+    BannerComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
